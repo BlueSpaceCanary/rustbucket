43a089f541d942e456f86135836a27e7f30af99a
Philip B Davis <phs.philip@gmail.com>
2018-09-10 14:45:00 -0700

    Copy example from crate for initial testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
+extern crate irc;
 mod factoid;
 
+use irc::client::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    // We can also load the Config at runtime via Config::load("path/to/config.toml")
+    let config = Config {
+        nickname: Some("sidra2".to_owned()),
+        server: Some("irc.crimes.international".to_owned()),
+        channels: Some(vec!["#test".to_owned()]),
+        ..Config::default()
+    };
+
+    let mut reactor = IrcReactor::new().unwrap();
+    let client = reactor.prepare_client_and_connect(&config).unwrap();
+    client.identify().unwrap();
+
+    reactor.register_client_with_handler(client, |client, message| {
+        print!("{}", message);
+        // And here we can do whatever we want with the messages.
+        Ok(())
+    });
+
+    reactor.run().unwrap();
 }
