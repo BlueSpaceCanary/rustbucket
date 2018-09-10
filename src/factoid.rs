9fb7e8cb84d3c87569e079237e956cc8099a090c
Philip B Davis <phs.philip@gmail.com>
2018-09-09 21:51:53 -0700

    Initial commit

---
+++ src/factoid.rs
@@ -0,0 +1,6 @@
+struct FactoidKey(string);
+struct FactoidValue(string);
+
+fn create_factoid(k: FactoidKey, v: FactoidValue) -> Result<FactoidValue, Error> {
+    // TODO save factoid lol
+}
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+mod factoid;
+
+fn main() {
+    println!("Hello, world!");
+}
