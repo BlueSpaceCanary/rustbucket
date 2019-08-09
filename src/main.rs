e44a3a50ca13eff91d0e9f7d06acf1a37cb3b0fc
Philip Davis <BlueSpaceCanary@users.noreply.github.com>
2019-08-08 22:54:52 -0700

    Ratelimiter (#12)
    Rate limit urself

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate irc;
+extern crate ratelimit;
 
 mod factoid;
 
 use crate::factoid::Brain;
 use irc::client::prelude::*;
 use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     // We can also load the Config at runtime via Config::load("path/to/config.toml")
@@ -23,11 +26,19 @@ fn main() {
     let client = reactor.prepare_client_and_connect(&config).unwrap();
     client.identify().unwrap();
 
+    let mut ratelimit = ratelimit::Builder::new()
+        .capacity(10) // number of tokens the bucket will hold
+        .quantum(5) // add five tokens per interval
+        .interval(Duration::new(30, 0)) // add `quantum` tokens every 30 seconds
+        .build();
+
+    let mut handle = ratelimit.make_handle();
+    thread::spawn(move || ratelimit.run());
+
     reactor.register_client_with_handler(client, move |client, message| {
         let cns = Arc::clone(&cns);
         let mut brain = cns.lock().unwrap();
-
-        connection_handler(config.clone(), client, message, &mut *brain);
+        connection_handler(config.clone(), client, message, &mut *brain, &mut handle);
         Ok(())
     });
 
@@ -39,14 +50,21 @@ fn connection_handler(
     client: &IrcClient,
     message: irc::proto::Message,
     brain: &mut Brain,
+    handle: &mut ratelimit::Handle,
 ) {
     // And here we can do whatever we want with the messages.
     if let Command::PRIVMSG(ref target, ref msg) = message.command {
         println!("{}", msg);
-        if let Some(resp) = brain.respond(msg) {
-            client
-                .send_privmsg(message.response_target().unwrap_or(target), resp)
-                .unwrap();
+        match handle.try_wait() {
+            Ok(()) => {
+                if let Some(resp) = brain.respond(msg) {
+                    client
+                        .send_privmsg(message.response_target().unwrap_or(target), resp)
+                        .unwrap();
+                };
+            }
+
+            _ => (), // If out of tokens, just skip it
         }
     }
 }
